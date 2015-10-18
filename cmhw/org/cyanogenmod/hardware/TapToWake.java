48d272edc267190f1154ba127b3c425e76bcc612
Flour_Mo <1561707299@qq.com>
2015-10-18 21:06:19 +0800

    Update double click wake configuration

--- cmhw/org/cyanogenmod/hardware/TapToWake.java
+++ cmhw/org/cyanogenmod/hardware/TapToWake.java
@@ -20,7 +20,7 @@ import org.cyanogenmod.hardware.util.FileUtils;
 
 public class TapToWake {
 
-    private static String CONTROL_PATH = "/sys/class/TP-UNLOCK/device/gesture";
+    private static String CONTROL_PATH = "/sys/devices/virtual/touchscreen/touchscreen_dev/gesture_ctrl";
 
     public static boolean isSupported() {
         return true;
